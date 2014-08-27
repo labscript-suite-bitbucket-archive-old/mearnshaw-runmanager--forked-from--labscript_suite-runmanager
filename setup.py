5feadb76e9bfe53e07d9e58598fbed21888a92ef
Matt Earnshaw <matt@earnshaw.org.uk>
2014-08-27 18:31:04 +0100

    add include_package_data to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 setup(
         name = "runmanager",
+        include_package_data = True,
         packages = find_packages(),
         install_requires = ['zprocess', 'qtutils']
 )
