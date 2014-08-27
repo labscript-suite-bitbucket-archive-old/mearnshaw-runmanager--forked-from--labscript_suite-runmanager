d8499a12905f36686b19750af5e698b5ec1d8dba
Matt Earnshaw <matt@earnshaw.org.uk>
2014-08-27 15:50:01 +0100

    restructure package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+        name = "runmanager",
+        packages = find_packages(),
+        install_requires = ['zprocess', 'qtutils', 'matplotlib']
+)
+
